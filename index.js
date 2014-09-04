930b8ba4ca62f585496677d8b9e21ef164786727
Chris Johnson <chris@crisistextline.org>
2014-09-04 18:15:46 -0400

    Add configuration to .env

--- index.js
+++ index.js
@@ -1,11 +1,21 @@
+var dotenv = require('dotenv');
 var Alerter = require('./lib/Alerter.js');
 var Checker = require('./lib/Checker.js');
 
-var checker = new Checker('HOST', /^ok=1/, {port: 443, path: '/'});
+dotenv.load();
+
+var checker = new Checker(
+    process.env.ALERTER_HOSTNAME,
+    new RegExp(process.env.ALERTER_REGEX),
+    {
+        port: process.env.ALERTER_PORT,
+        path: process.env.ALERTER_PATH
+    }
+);
 
 setInterval(function () {
     checker.check(function () {
         Alerter.alert()
     });
-}, 30000);
+}, 3000);
 
